691ab07fdb4f73b2f0320e9cfe36958baee76033
Aakash Rana <163324161+SovietComrade777@users.noreply.github.com>
2024-07-06 23:49:53 +0530

    Create q9.c

---
+++ q9.c
@@ -0,0 +1,10 @@
+/*Write a C program that accepts two integers from the user and calculates the sum of the two integers.*/#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Input the first Integer:\n");
+    scanf("%d",&a);
+    printf("Input the second Integer:\n");
+    scanf("%d",&b);
+    printf("Sum of the above two integers =%d",a+b);
+    return 0;
+}
