aba85ed5876bca5e1c1c359f1faa276a85b58d8e
Aakash Rana <163324161+SovietComrade777@users.noreply.github.com>
2024-07-11 23:36:38 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int q,x;
+    
+    scanf("%d%d",&q,&x);
+    // scanf("%d",x);
+    if(q%x==0){
+        printf("%d is a multiple of %d",q,x);
+    }
+    else{
+        printf("%d is a multiple of %d",q,x);
+    }
+    return 0;
+}
